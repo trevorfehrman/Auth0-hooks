46e46c5536ccce9bf334aa6512e83a4b7a183f57
Trevor Fehrman <trevorfehrman@gmail.com>
2019-06-02 14:12:35 -0700

    toy proj complete

---
+++ react-auth0-popup/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import TvShows from './components/TvShows.js';
+import Movies from './components/Movies.js';
+import auth from './auth/service';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    auth.loginCallback = this.loggedIn.bind(this);
+    auth.logoutCallback = this.loggedOut.bind(this);
+
+    this.state = { loggedIn: false };
+  }
+
+  loggedIn() {
+    this.setState({ loggedIn: true });
+  }
+
+  loggedOut() {
+    this.setState({ loggedIn: false });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? <Movies /> : <TvShows />}
+        {this.state.loggedIn ? (
+          <button onClick={() => auth.logout()} className='log-in'>
+            Log Out
+          </button>
+        ) : (
+          <button onClick={() => auth.login()} className='log-in'>
+            Log In
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
